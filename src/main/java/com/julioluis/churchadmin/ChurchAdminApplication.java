427e6b69476de9a6b255adf14b3c7e8cd67f5f30
jpelegrino <jpelegrino25@gmail.com>
2020-11-26 07:59:20 -0600

    Project configuration, Liquibase config, Adding the database squema.

---
+++ src/main/java/com/julioluis/churchadmin/ChurchAdminApplication.java
@@ -0,0 +1,13 @@
+package com.julioluis.churchadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/julioluis/churchadmin/ChurchAdminApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.julioluis.churchadmin;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ChurchAdminApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
